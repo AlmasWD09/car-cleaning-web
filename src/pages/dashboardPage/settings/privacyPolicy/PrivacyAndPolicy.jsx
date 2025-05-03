079f1b1dac92664530debd52a729002e968d4824
Almas Hossain <almashossain7384@gmail.com>
2025-05-03 12:30:25 +0600

    dashboard privacy policy section jodit text editor add ok

--- src/pages/dashboardPage/settings/privacyPolicy/PrivacyAndPolicy.jsx
+++ src/pages/dashboardPage/settings/privacyPolicy/PrivacyAndPolicy.jsx
@@ -1,9 +1,46 @@
+import { Button } from "antd";
+import JoditEditor from "jodit-react";
+import { useRef, useState } from "react";
 
 
 const PrivacyAndPolicy = () => {
+  const [content, setContent] = useState('');
+  const editor = useRef(null);
+
+
+  const handleUpdate = () => {
+    console.log('cickk')
+  }
   return (
-    <div>PrivacyAndPolicy</div>
+    <>
+      <div className="w-full mt-6">
+        <JoditEditor
+          ref={editor}
+          value={content}
+          // config={{
+          //   height: "400px", // Set your desired height
+          // }}
+          onChange={(newContent) => {
+            console.log("Editor Content:", newContent);
+            setContent(newContent);
+          }}
+        />
+      </div>
+
+      <div className="flex justify-end ">
+        <Button
+          htmlType="submit"
+          style={{ backgroundColor: "#1E73BE", color: "white", fontFamily: "degularDisplay", padding: "24px 40px", fontSize: "16px", fontWeight: "bold", margin: "10px 0px" }}
+          onClick={handleUpdate}
+        >
+          Save
+        </Button>
+      </div>
+    </>
   )
 }
 
 export default PrivacyAndPolicy
+
+
+
