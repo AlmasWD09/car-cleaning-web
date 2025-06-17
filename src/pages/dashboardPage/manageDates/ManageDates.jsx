3a7e8ca34eac25440dd08255a4efc5e5044bb732
almas <almashossain7384@gmail.com>
2025-06-17 19:27:27 +0600

    delete api done

--- src/pages/dashboardPage/manageDates/ManageDates.jsx
+++ src/pages/dashboardPage/manageDates/ManageDates.jsx
@@ -1,39 +1,89 @@
 
 
-import { Modal } from 'antd';
+import { Button, Form, Modal } from 'antd';
 import { useForm } from 'antd/es/form/Form';
 import { useState } from 'react';
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
 import { useAddBlockDateApiMutation, useDeleteUnBlockDateApiMutation, useGetBlockedDateApiQuery } from '../../../redux/dashboardFeatures/manageDate/dashboardManageDateApi';
+import toast from 'react-hot-toast';
 
 
 const ManageDates = () => {
-  const [value, onChange] = useState(new Date());
   const [formOne] = useForm()
   const [formTwo] = useForm()
-  const [mondalOne, setModalOne] = useState(false);
-  const [selected, setSelected] = useState([]);
+  const [modalOne, setModalOne] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
 
   const { data: manageDateData } = useGetBlockedDateApiQuery() // get
   const [addBlockDateApi] = useAddBlockDateApiMutation() // post
   const [deleteUnBlockDateApi] = useDeleteUnBlockDateApiMutation() // delete
+  const allDateData = manageDateData?.data?.data
 
 
+
+  const today = new Date();
+  const disabledBefore = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+
+  // ✅ Local date format function (YYYY-MM-DD)
+  const formatDateLocal = (date) => {
+    const year = date.getFullYear();
+    const month = `${date.getMonth() + 1}`.padStart(2, "0");
+    const day = `${date.getDate()}`.padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+
+  // 🔹 User date select korle modal open
+  const handleDateSelect = (date) => {
+    if (date) {
+      setSelectedDate(date);
+      setModalOne(true);
+    }
+  };
+  const year = selectedDate?.getFullYear();
+  const month = String(selectedDate?.getMonth() + 1).padStart(2, '0'); // getMonth is 0-based
+  const day = String(selectedDate?.getDate()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day}`;
 
 
 
   // =============  modal one start ===============
-  const onFinishOne = () => {
-    console.log('click')
-  }
 
-  const showModalOne = (record) => {
-    setModalOne(true)
+  const onFinishOne = async () => {
+    const formData = new FormData();
+    formData.append("date", formattedDate);
+
+    // formData.forEach((value, key) => {
+    //   console.log(key, value);
+    // });
+
+    try {
+      const res = await addBlockDateApi(formData).unwrap();
+      console.log(res);
+
+      if (res?.status === true) {
+        toast.success(res?.message);
+        setModalOne(false);
+      }
+    } catch (error) {
+      const errorMessage = error?.data?.message;
+      if (typeof errorMessage === 'object') {
+        Object.entries(errorMessage).forEach(([field, messages]) => {
+          if (Array.isArray(messages)) {
+            messages.forEach(msg => toast.error(msg));
+          } else {
+            toast.error(messages);
+          }
+        });
+      } else {
+        toast.error(errorMessage);
+      }
+    }
   }
 
   const handleModalOneOk = () => {
-
+    formOne.submit()
   }
 
   const handleCancelModalOne = () => {
@@ -42,83 +92,61 @@ const ManageDates = () => {
   // =============  modal one end ===============
 
 
+  const handleDelete = (id) => {
+    console.log(id)
+  }
+
+
+  
 
   return (
     <div>
-      <div className='flex justify-evenly'>
+      <div className='flex justify-evenly gap-10 pt-14'>
 
-        <div className='w-[40%]'>
-          <h2 className='text-[28px] font-degular font-medium'>Date Blocked</h2>
-          <div className="bg-gray-100 shadow-md p-4">
+        <div className='w-[60%]'>
+          <h2 className='text-[28px] font-degular font-medium pb-4'>Block certain dates</h2>
+          <div className="bg-gray-100 shadow-md p-4 h-[340px]">
             <DayPicker
-              mode="multiple"
-              selected={selected}
-              onSelect={setSelected}
+              mode="single"
+              selected={selectedDate}
+              onSelect={handleDateSelect}
+              disabled={{ before: disabledBefore }}
               modifiersClassNames={{
-                selected: 'my-selected'
-              }}
-              modifiersStyles={{
-                selected: {
-                  color: 'red',
-                  border: 'none',
-                  backgroundColor: 'transparent',
-                  fontWeight: 'bold'
-                }
+                disabled: "cursor-not-allowed opacity-50",
               }}
             />
-            <div className="mt-2">
-              {selected?.length > 0 ? (
-                <p>
-                  Selected dates:{" "}
-                  {selected.map((date) => date.toLocaleDateString()).join(", ")}
-                </p>
-              ) : (
-                <p>Pick one or more days.</p>
-              )}
-            </div>
           </div>
         </div>
 
-        <div className='w-[30%]'>
+        <div className='w-[50%]'>
           <div>
-            <h2 className='text-[28px] font-degular font-medium text-red-500 flex items-center gap-3'>
+            <h2 className='text-[28px] font-degular font-medium text-red-500 flex items-center gap-3 pb-4'>
               <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5.75017 23.25L23.2502 5.75001M27 14.5C27 21.4036 21.4036 27 14.5 27C7.59644 27 2 21.4036 2 14.5C2 7.59644 7.59644 2 14.5 2C21.4036 2 27 7.59644 27 14.5Z" stroke="#FF3636" stroke-width="3" />
               </svg>
 
-              Block certain dates</h2>
+              Date Blocked</h2>
           </div>
-          <div className='border border-[#ccc] rounded-[20px] p-4 space-y-4'>
-            <div className='flex justify-between items-center'>
-              <p className='text-[20px] font-degular font-medium'>20 March, 2025</p>
-              <button className='bg-primary py-2 px-10 rounded-[20px] text-[16px] text-[#fff]'>Unlock</button>
-            </div>
-            <div className='flex justify-between items-center'>
-              <p className='text-[20px] font-degular font-medium'>20 March, 2025</p>
-              <button className='bg-primary py-2 px-10 rounded-[20px] text-[16px] text-[#fff]'>Unlock</button>
-            </div>
-            <div className='flex justify-between items-center'>
-              <p className='text-[20px] font-degular font-medium'>20 March, 2025</p>
-              <button className='bg-primary py-2 px-10 rounded-[20px] text-[16px] text-[#fff]'>Unlock</button>
-            </div>
-            <div className='flex justify-between items-center'>
-              <p className='text-[20px] font-degular font-medium'>20 March, 2025</p>
-              <button className='bg-primary py-2 px-10 rounded-[20px] text-[16px] text-[#fff]'>Unlock</button>
-            </div>
-            <div className='flex justify-between items-center'>
-              <p className='text-[20px] font-degular font-medium'>20 March, 2025</p>
-              <button className='bg-primary py-2 px-10 rounded-[20px] text-[16px] text-[#fff]'>Unlock</button>
+          <div className='border border-[#ccc] rounded-[20px]'>
+            <div className=' space-y-4 h-[340px] overflow-y-auto p-6'>
+              {
+                allDateData?.map((singleData, index) => {
+                  return (
+                    <div key={index}
+                      className='flex justify-between items-center'
+                    >
+                      <p className='text-[20px] font-semibold font-degular'>{singleData?.date}</p>
+                      <button
+                        onClick={() => handleDelete(singleData?.id)}
+                        className='bg-primary py-2 px-10 rounded-[20px] text-[16px] text-[#fff]'>Unlock</button>
+                    </div>
+                  )
+                })
+              }
             </div>
           </div>
         </div>
-
       </div>
-
-
-      <div className='my-4  hidden'>
-        <button onClick={() => showModalOne()} className='bg-primary text-[#fff] py-2 px-6 rounded-lg'>Click Here</button>
-      </div>
-
 
 
       {/* modal components */}
@@ -127,10 +155,10 @@ const ManageDates = () => {
         centered
         title={
           <div className="text-center bg-red-600 text-[#ffffff] py-4 font-degular text-[18px]  font-semibold rounded-t-lg">
-            User details
+            Block Date
           </div>
         }
-        open={mondalOne}
+        open={modalOne}
         onOk={handleModalOneOk}
         onCancel={handleCancelModalOne}
         footer={null}
@@ -139,24 +167,27 @@ const ManageDates = () => {
         maskStyle={{ backgroundColor: 'rgba(134, 134, 134, 0.4)' }}
       >
 
-        <div className="p-8 space-y-4">
-          <span className='flex justify-center'>
-            <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M10.4004 49.6L49.6004 10.4M58 30C58 45.464 45.464 58 30 58C14.536 58 2 45.464 2 30C2 14.536 14.536 2 30 2C45.464 2 58 14.536 58 30Z" stroke="#FF3636" stroke-width="4" />
-            </svg>
+        <Form form={formOne} onFinish={onFinishOne}>
+          <div className="p-8 space-y-4">
+            <p className='py-8'>{formattedDate}</p>
+            <span className='flex justify-center'>
+              <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10.4004 49.6L49.6004 10.4M58 30C58 45.464 45.464 58 30 58C14.536 58 2 45.464 2 30C2 14.536 14.536 2 30 2C45.464 2 58 14.536 58 30Z" stroke="#FF3636" stroke-width="4" />
+              </svg>
 
-          </span>
+            </span>
 
-          <div>
-            <p className='font-degular text-center font-semibold text-[28px]'>Are you sure to <span className='text-[#FF3F3F] font-bold'>Block</span>
-              <br />
-              this date ?</p>
+            <div>
+              <p className='font-degular text-center font-semibold text-[28px]'>Are you sure to <span className='text-[#FF3F3F] font-bold'>Block</span>
+                <br />
+                this date ?</p>
+            </div>
+
+            <div className='flex justify-center mt-2'>
+              <button type='submit' className='bg-[#FF3F3F] text-[#fff] text-[16px] font-semibold px-4 py-3 rounded-xl w-[300px]'>Block</button>
+            </div>
           </div>
-
-          <div className='flex justify-center mt-2'>
-            <button className='bg-[#FF3F3F] text-[#fff] text-[16px] font-semibold px-4 py-3 rounded-xl w-[300px]'>Block</button>
-          </div>
-        </div>
+        </Form>
       </Modal>
     </div>
   );
